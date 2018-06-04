a0f2f4bf9365230740d3ee7e9b06213583e3a413
Oleg Volkov <olegv@parallels.com>
2018-06-04 14:07:36 +0300

    draw_begin/end added to abstract display API

--- RGB16DisplayAdaptor.h
+++ RGB16DisplayAdaptor.h
@@ -26,17 +26,37 @@ public:
 	/* Enable / disable display */
 	virtual void enable(bool on) = 0;
 
+	/*
+	 * Core drawing API
+	 */
+
+	/* Begin / end drawing operations */
+	virtual void draw_begin() = 0;
+	virtual void draw_end() = 0;
+
+	/*
+	 * The following drawing operations with names ended with _ should be enclosed
+	 * between draw_begin(), draw_end() calls
+	 */
+
 	/* Put pixel */
-	virtual void put_pixel(uint16_t x, uint16_t y, uint16_t colour) = 0;
+	virtual void put_pixel_(uint16_t x, uint16_t y, uint16_t colour) = 0;
 
 	/* Draw horizontal line */
-	virtual void hline(uint16_t x, uint16_t y, uint16_t len, uint16_t colour) = 0;
+	virtual void hline_(uint16_t x, uint16_t y, uint16_t len, uint16_t colour) = 0;
 
 	/* Draw vertical line */
-	virtual void vline(uint16_t x, uint16_t y, uint16_t len, uint16_t colour) = 0;
-	
+	virtual void vline_(uint16_t x, uint16_t y, uint16_t len, uint16_t colour) = 0;
+
+	/* Fill certain region */
+	virtual void fill_rect_(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t colour) = 0;
+
+	/*
+	 * Display RAM writing API
+	 */
+
 	/* Setup rectangular writing area */
-	virtual void write_begin(uint16_t x, uint16_t y, uint16_t w, uint16_t h, bool col_order) = 0;
+	virtual void write_begin_(uint16_t x, uint16_t y, uint16_t w, uint16_t h, bool col_order) = 0;
 
 	/* Write pixels */
 	virtual void write_pixels(uint16_t const* pix_buff, int len) = 0;
@@ -45,13 +65,54 @@ public:
 	virtual void write_pixels_bm(uint8_t const* pix_bm, int len, uint16_t colours[2]) = 0;
 
 	/* End writing */
-	virtual void write_end() = 0;
+	virtual void write_end_() = 0;
+
+	/*
+	 * Drawing routines implementation
+	 */
+
+	/* Setup rectangular writing area */
+	virtual void write_begin(uint16_t x, uint16_t y, uint16_t w, uint16_t h, bool col_order) {
+		draw_begin();
+		write_begin_(x, y, w, h, col_order);
+	}
+
+	/* End writing */
+	virtual void write_end() {
+		write_end_();
+		draw_end();
+	}
+
+	/* Put pixel */
+	void put_pixel(uint16_t x, uint16_t y, uint16_t colour) {
+		draw_begin();
+		put_pixel_(x, y, colour);
+		draw_end();
+	}
+
+	/* Draw horizontal line */
+	void hline(uint16_t x, uint16_t y, uint16_t len, uint16_t colour) {
+		draw_begin();
+		hline_(x, y, len, colour);
+		draw_end();
+	}
+
+	/* Draw vertical line */
+	void vline(uint16_t x, uint16_t y, uint16_t len, uint16_t colour) {
+		draw_begin();
+		vline_(x, y, len, colour);
+		draw_end();
+	}
 
 	/* Fill certain region */
-	virtual void fill_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t colour) = 0;
+	void fill_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t colour) {
+		draw_begin();
+		fill_rect_(x, y, w, h, colour);
+		draw_end();
+	}
 
 	/* Fill display memory with given colour */
-	virtual void fill(uint16_t colour) {
+	void fill(uint16_t colour) {
 		fill_rect(0, 0, width(), height(), colour);
 	}
 };
