1b19d06308b1a62759c75d7692c2900cdb66831e
Oleg Volkov <olegv@parallels.com>
2020-01-16 23:31:15 +0300

    Font with limited symbol set to reduce memory footprint

--- glcd_fonts.h
+++ glcd_fonts.h
@@ -11,8 +11,10 @@ extern struct glcd_font g_font_Lucida12x12Cyr;
 
 /* Latin fonts */
 extern struct glcd_font g_font_Tahoma12x11Bld;
+extern struct glcd_font g_font_Tahoma12x11BldD; /* Digits and special symbols only */
 extern struct glcd_font g_font_Tahoma15x16;
 extern struct glcd_font g_font_Tahoma19x20;
+extern struct glcd_font g_font_Tahoma19x20dg; /* Digits only */
 extern struct glcd_font g_font_Tahoma34x38;
 
 /* Large font with only numerical and some special symbols in code the range 32..63 */
