4ecbfd5d81f0bfdce3fb401dd087d0448eef49e8
Oleg Volkov <olegv142@gmail.com>
2024-03-17 21:28:52 +0300

    ST7789 examples

---
+++ examples/ST7789_bubbles/ST7789_bubbles.ino
@@ -0,0 +1,23 @@
+#include "GenRGB16Adaptor.h"
+#include "draw.h"
+
+GenRGB16Adaptor g_display(240, 320, 0, 10, 19, 18);
+
+void setup() {
+  // put your setup code here, to run once:
+  g_display.set_flip(true, false);
+  g_display.set_inversion(true);
+  g_display.set_bgr(false);
+
+  g_display.begin();
+  g_display.init();
+}
+
+void loop() {
+  unsigned ra = random(4, 64);
+  unsigned x = random(ra, g_display.width() - ra);
+  unsigned y = random(ra, g_display.height() - ra);
+  unsigned r = 63 * random(0, 5), g = 63 * random(0, 5), b = 63 * random(0, 5);
+  draw_circle(&g_display, x, y, ra, 0, RGB16(r, g, b));
+}
+
---
+++ examples/ST7789_text_orientation/ST7789_text_orientation.ino
@@ -0,0 +1,35 @@
+#include "GenRGB16Adaptor.h"
+#include "glcd_fonts.h"
+
+GenRGB16Adaptor g_display(240, 320, 2, 10, 19, 18);
+
+void print_text(uint8_t o)
+{
+  uint16_t colors[] = {RGB16Black, RGB16Green};
+  String s = "Orientation ";
+  s += o;
+  g_display.rotate_axis(o);
+  glcd_print_str(&g_display, 10, 20, s.c_str(), &g_font_Tahoma15x16, 1, colors);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  g_display.set_flip(true, false);
+  g_display.set_inversion(true);
+  g_display.set_bgr(false);
+
+  g_display.begin();
+  g_display.init();
+
+  print_text(0);
+  print_text(1);
+  print_text(2);
+  print_text(3);  
+}
+
+void loop() {
+}
+
+
+
+
