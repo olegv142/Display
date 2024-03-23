5e701fe6b8638103a0516587464f66882cb7be2a
Oleg Volkov <olegv142@gmail.com>
2024-03-23 21:44:34 +0300

    Examples moved

---
+++ examples/ILI9341/Colors/Colors.ino
@@ -0,0 +1,33 @@
+#include "GenRGB16Adaptor.h"
+
+GenRGB16Adaptor g_display(240, 320, 0, 10, 19, 18);
+
+void setup() {
+  // put your setup code here, to run once:
+  g_display.set_bgr(true);
+  g_display.begin();
+  g_display.init();
+  
+  unsigned const h = g_display.height();
+  unsigned const w = g_display.width();
+  unsigned const b = h / 5;
+
+  unsigned i;
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, i, w, RGB16(255, 255*i/b, 0));
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, b + i, w, RGB16(255*(b-i)/b, 255, 0));
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, b + b + i, w, RGB16(0, 255, 255*i/b));
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, b + b + b + i, w, RGB16(0, 255*(b-i)/b, 255));
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, b + b + b + b + i, w, RGB16(255*i/b, 0, 255));
+}
+
+void loop() {
+}
+
+
+
+
