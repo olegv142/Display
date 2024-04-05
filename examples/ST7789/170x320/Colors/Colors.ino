75808cbb1c95ef66bdae054b34ad9ea58374257b
Oleg Volkov <olegv142@gmail.com>
2024-04-06 00:00:53 +0300

    170x320 screen examples

---
+++ examples/ST7789/170x320/Colors/Colors.ino
@@ -0,0 +1,36 @@
+#include "GenRGB16Adaptor.h"
+
+GenRGB16Adaptor g_display(170, 320, 0, 10, 19, 18);
+
+void setup() {
+  // put your setup code here, to run once:
+  g_display.set_flip(true, false);
+  g_display.set_padding(35, 35, 0, 0);
+  g_display.set_inversion(true);
+
+  g_display.begin();
+  g_display.init();
+  
+  unsigned const h = g_display.height();
+  unsigned const w = g_display.width();
+  unsigned const b = h / 5;
+
+  unsigned i;
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, i, w, RGB16(255, 255*i/b, 0));
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, b + i, w, RGB16(255*(b-i)/b, 255, 0));
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, b + b + i, w, RGB16(0, 255, 255*i/b));
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, b + b + b + i, w, RGB16(0, 255*(b-i)/b, 255));
+  for (i = 0; i < b; ++i)
+    g_display.hline(0, b + b + b + b + i, w, RGB16(255*i/b, 0, 255));
+}
+
+void loop() {
+}
+
+
+
+
